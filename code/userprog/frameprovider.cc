75492e445da87ccf60b0e916305c33f521bf97b6
Snowtuss <ironislam@hotail.com>
2020-01-20 14:28:11 +0100

    Step 4 progress - Adding a squeleton for FrameProvider file

---
+++ code/userprog/frameprovider.cc
@@ -0,0 +1,30 @@
+#include "copyright.h"
+#include "system.h"
+#include "bitmap.h"
+#include "frameprovider.h"
+
+
+FrameProvider::FrameProvider(int size)
+{
+	
+}
+
+FrameProvider::~FrameProvider()
+{
+
+}
+
+void SynchConsole::GetEmptyFrame()
+{
+
+}
+
+void SynchConsole::ReleaseFrame()
+{
+
+}
+
+void SynchConsole::NumAvailFrame()
+{
+
+}
